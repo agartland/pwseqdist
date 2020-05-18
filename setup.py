55bdcd8a0263e254de6d61ecd47fad077593031d
kmayerb <kmayerb@gmail.com>
2020-05-18 15:47:51 -0700

    edit setup.py for 0.1.1 w/PyPI README.md

--- setup.py
+++ setup.py
@@ -7,16 +7,24 @@ For more information, see https://stackoverflow.com/questions/1471994/what-is-se
 from setuptools import setup, find_packages
 PACKAGES = find_packages()
 
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 opts = dict(name='pwseqdist',
             maintainer='Andrew Fiore-Gartland',
             maintainer_email='agartlan@fredhutch.org',
             description='Efficiently computes distances between protein sequences',
-            long_description=("""A small package that efficiently computes distances between protein sequences. Can accommodate similarity matrices, sequences of different lengths and custom metrics."""),
+            long_description=long_description,
+            long_description_content_type='text/markdown',
             url='https://github.com/agartland/pwseqdist',
             license='MIT',
             author='Andrew Fiore-Gartland',
             author_email='agartlan@fredhutch.org',
-            version='0.1.0',
+            version='0.1.1',
             packages=PACKAGES
            )
 
